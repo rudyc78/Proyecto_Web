9c84897e4b05ebc52ca0c903327ec25159258959
rudyc78 <rudycabrera.ma@gmail.com>
2023-03-12 23:28:52 -0600

    Pagina de registro con base de datos

--- Web/store/models.py
+++ Web/store/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class alumno(models.Model):
+"""class alumno(models.Model):
     nombre = models.CharField(max_length=200)
     apellido = models.CharField(max_length=200)
 
@@ -9,3 +9,6 @@ class materia(models.Model):
     subject = models.CharField(max_length=300)
     score = models.CharField(max_length=10)
     
+class login(models.Model):
+    subject = models.CharField(max_length=300)
+    score = models.CharField(max_length=10)"""
